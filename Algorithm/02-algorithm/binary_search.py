5910d9028aff163a46d0a3091aad82d393e8c910
yoshlikmedia <yoshlikmedia@gmail.com>
2022-02-10 11:39:12 +0500

    binary_search.py update

--- Algorithm/02-algorithm/binary_search.py
+++ Algorithm/02-algorithm/binary_search.py
@@ -1,9 +1,14 @@
+from typing import List
+
+# Example 1:
 def binary_search(list, item):
     low = 0
     high = len(list)-1
+
     while low <= high:
         mid = (low + high)//2
         guess = list[mid]
+
         if guess == item:
             return mid
         if guess > item:
@@ -11,3 +16,24 @@ def binary_search(list, item):
         else:
             low = mid + 1
     return None
+
+
+# Example 2:
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            temp = nums[mid]
+
+            if temp == target:
+                return mid
+
+            if temp > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return None
