a7292831ef4cec0ee289cae238bfbf8761253d1e
yoshlikmedia <yoshlikmedia@gmail.com>
2022-03-27 20:17:53 +0500

    pandas

--- Docker/install.sh
+++ Docker/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env
 
 # Docker install script
 
--- Vision/Recognition-UI/main.py
+++ Vision/Recognition-UI/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import cv2
 import numpy as np
 import os
@@ -14,22 +16,21 @@ x1, y1, x2 = 0, 0, 600
 known_people_list = []
 
 
-
 # face_image = cv2.imread('face.png')
 # accept_image = cv2.imread('accept.png')
 # reject_image = cv2.imread('reject.png')
 
 def load_known_faces():
-    known_faces="me/"
+    known_faces = "me/"
     list_images = os.listdir(known_faces)
     for img_name in tqdm(list_images):
         person_name = img_name.split(".")[0]
         image_path = os.path.join(known_faces, img_name)
         image = cv2.imread(image_path)
         face_encodings = app.get(image)
-        if len(face_encodings)==0:
+        if len(face_encodings) == 0:
             print("Cannot extract face feature from : {}".format(img_name))
-            continue;
+            continue
         person = {}
         person["name"] = person_name
         person["embedding"] = face_encodings[0].embedding
@@ -64,21 +65,21 @@ def line_down_frame(image, line):
 
         result = cv2.addWeighted(image, 1-alpha/100, result, alpha/100, 0)
 
-
     return result, line
+
 
 def find_face(embedding):
     best_matches = []
-    
+
     for person in known_people_list:
         sim = 1 - cosine(person["embedding"], embedding)
-        if(sim>0.5):
+        if(sim > 0.5):
             known = {}
-            known["person"]=person
-            known["sim"]=sim
+            known["person"] = person
+            known["sim"] = sim
             best_matches.append(known)
     if len(best_matches) == 0:
-        return 
+        return
     best_matches.sort(key=key_func)
     return best_matches[0]
 
@@ -94,7 +95,8 @@ def main():
         _, frame = cap.read()
 
         cv2.namedWindow('img', cv2.WINDOW_FREERATIO)
-        cv2.setWindowProperty('img', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.setWindowProperty(
+            'img', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         # print(_)
 
         result = resize_frame(frame)
@@ -104,33 +106,33 @@ def main():
         for face in faces:
             # print(face)
             bbox = face.bbox
-            x1,y1,x2,y2 = bbox[0], bbox[1], bbox[2], bbox[3]
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0,255,0), 1)
+            x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]
+            cv2.rectangle(frame, (int(x1), int(y1)),
+                          (int(x2), int(y2)), (0, 255, 0), 1)
             person = find_face(face.embedding)
 
             if person is None:
                 continue
-            
-            print("recognized as: {}, similarity: {}".format(person["person"]["name"], person["sim"]))
+
+            print("recognized as: {}, similarity: {}".format(
+                person["person"]["name"], person["sim"]))
 
         result, line = line_down_frame(result, line)
-
 
         cv2.imshow("img", result)
 
         k = cv2.waitKey(10)
-        if k==ord("q"):
+        if k == ord("q"):
             cv2.destroyAllWindows()
             # cap.release()
             break
-    
+
 
 # while True:
 #     ret, frame = cap.read()
 
 #     if not ret:
 #         break
-
 
 
 #     frame = resize_frame(frame)
@@ -151,27 +153,17 @@ def main():
 #     #             1, (36, 255, 12), thickness=2, lineType=8)
 
 
-
-
 #     cv2.imshow('frame', result)
 
 #     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         break
 
 
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
-    app = FaceAnalysis(name='buffalo_s',providers=['CUDAExecutionProvider', 'CPUExecutionProvider'], allowed_modules=['detection', 'recognition'])
+    app = FaceAnalysis(name='buffalo_s', providers=[
+                       'CUDAExecutionProvider', 'CPUExecutionProvider'], allowed_modules=['detection', 'recognition'])
     app.prepare(ctx_id=0, det_size=(640, 640))
-    
+
     main()
     # img = ins_get_image('test')
     # faces = app.get(img)
